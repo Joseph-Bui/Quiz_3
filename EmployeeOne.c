c35b5559a6d7d9f459f995816b9e3a77b8714363
Joseph Bui <jhbui@cpp.edu>
2022-10-19 20:06:09 -0700

    adding search by number and salary

---
+++ EmployeeOne.c
@@ -0,0 +1,50 @@
+#include <string.h> 
+#include "Employee.h" 
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++)//search until end of table  ptr++ will increment by what?? 
+    { 
+        if(ptr->number == targetNumber)//check if it equals the Employee number 
+            return (PtrToEmployee) ptr;//return location of the number to callee. 
+    } 
+    return NULL;//this will only happen if no Employee number matches in loop above 
+} 
+ 
+//Essentially the same functionality as above but comparing strings to check if equal 
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char * targetName){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++) 
+    { 
+        if(strcmp(ptr->name,targetName) == 0) 
+            return (PtrToEmployee) ptr; 
+    } 
+    return NULL; 
+} 
+
+//Searching by Phone number
+PtrToEmployee searchEmployeeByPhoneNumer(PtrToConstEmployee ptr, int tableSize, char * targetPhoneNumber)
+{
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++)
+    {
+        if(ptr->phone == targetPhoneNumber)
+        {
+            return(PtrToEmployee) ptr;
+        }
+    }
+    return NULL;
+}
+
+//Searching by Salary
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary)
+{
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)
+    {
+        if(ptr -> salary == targetSalary)
+        {
+            return(PtrToEmployee) ptr;
+        }
+    }
+    return NULL;
+}
